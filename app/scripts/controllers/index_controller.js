5eb09b54791fabcafc9619dbc353fc107c8929e7
milica <milica.kadic@prelovac.com>
2014-01-28 18:42:29 +0100

    node integration

---
+++ app/scripts/controllers/index_controller.js
@@ -0,0 +1,19 @@
+Proto.IndexController = Ember.ArrayController.extend({
+    actions: {
+        createNewProject: function () {
+
+            $.post("/api/projects", {
+                "name": "Project 1",
+                "elements": [{id: 'test'}, {id: 'test2'}]
+            }, function (data, textStatus, jqXHR) {
+
+                console.log("Post resposne:");
+                console.dir(data);
+                console.log(textStatus);
+                console.dir(jqXHR);
+
+            });
+
+        }
+    }
+});
--- node/app.js
+++ node/app.js
@@ -1,13 +1,128 @@
-var express = require('express');
+var application_root = __dirname,
+    express = require("express"),
+    path = require("path"),
+    mongoose = require('mongoose');
+
 var app = express();
 
-app.get('/', function(req, res) {
+// Database
 
-    var body = 'Hello World';
-    res.setHeader('Content-Type', 'text/plain');
-    res.setHeader('Content-Length', Buffer.byteLength(body));
-    res.end(body);
+mongoose.connect('mongodb://localhost/proto_database');
+
+// Config
+
+app.set('port', process.env.VCAP_APP_PORT || 3000);
+
+app.configure(function () {
+    //app.engine('html', require('ejs').renderFile);
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(path.join(application_root, "../app")));
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+
 });
 
-app.listen(9000);
-console.log('Listening on port 9000');
+var Schema = mongoose.Schema;
+
+var Project = new Schema({
+    name: { type: String, required: true },
+    elements: { type: [], required: false }
+});
+
+
+var ProjectModel = mongoose.model('Project', Project);
+
+app.get('/api', function (req, res) {
+    res.send('Proto API is running');
+});
+
+app.get('/api/projects', function (req, res) {
+
+    return ProjectModel.find(function (err, projects) {
+        if (!err) {
+            return res.send(projects);
+        } else {
+            return console.log(err);
+        }
+    });
+
+});
+
+app.post('/api/projects', function (req, res) {
+
+    var project;
+
+    console.log("POST: ");
+    console.log(req.body);
+
+    project = new ProjectModel({
+        name: req.body.name,
+        elements: req.body.elements
+    });
+
+    project.save(function (err) {
+        if (!err) {
+            return console.log("created");
+        } else {
+            return console.log(err);
+        }
+    });
+
+    return res.send(project);
+});
+
+app.get('/api/projects/:id', function (req, res) {
+
+    return ProjectModel.findById(req.params.id, function (err, project) {
+        if (!err) {
+            return res.send(project);
+        } else {
+            return console.log(err);
+        }
+    });
+
+});
+
+app.put('/api/projects/:id', function (req, res) {
+
+    return ProjectModel.findById(req.params.id, function (err, project) {
+
+        project.name = req.body.name;
+        project.elements = req.body.elements;
+
+        return project.save(function (err) {
+            if (!err) {
+                console.log("updated");
+            } else {
+                console.log(err);
+            }
+            return res.send(project);
+        });
+    });
+});
+
+app.delete('/api/projects/:id', function (req, res) {
+
+    return ProjectModel.findById(req.params.id, function (err, project) {
+
+        return project.remove(function (err) {
+            if (!err) {
+                console.log("removed");
+                return res.send('');
+            } else {
+                console.log(err);
+            }
+        });
+
+    });
+
+});
+
+// Launch
+
+//module.exports = app;
+
+app.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
+});
